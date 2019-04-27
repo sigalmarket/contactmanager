771d3d39fc52448da815b096783ee0ca3d3c4c81
sigalmarket <anthony.vella.2409.2@gmail.com>
2019-04-27 15:14:26 +0200

    Updates

---
+++ precache-manifest.60c4dc93f672dba1cdb99a20e7957c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b604192e4b919f9988a3",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b604192e4b919f9988a3",
+    "url": "/contactmanager/static/js/main.5d56fc23.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "f99c248c25b7f22ce44b",
+    "url": "/contactmanager/static/css/2.a2018699.chunk.css"
+  },
+  {
+    "revision": "f99c248c25b7f22ce44b",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "1ee9bbc6400d3face19f76893fa28520",
+    "url": "/contactmanager/index.html"
+  }
+];
